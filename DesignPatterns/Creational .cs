1e931a8b037115db8b1e6e5dd808242004fe363c
Mehe_od76@itstep.edu.az <Mehe_od76@itstep.edu.az>
2022-11-23 00:41:19 +0400

    Добавьте файлы проекта.

---
+++ DesignPatterns/Creational .cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//SingletonPattern
+
+
+
+
+//namespace Design_Patternsl;
+//
+//public sealed class President
+//{
+//
+//    private President()
+//    {
+//     
+//    }
+//
+//    private static President Instance;
+//
+//    public static President GetInstance()
+//    {
+//        if (Instance == null)
+//        {
+//            Instance = new President();
+//        }
+//        return Instance;
+//    }
+//
+//    public void Someworks()
+//    {
+//
+//    }
+//}
+//
+//class Program
+//{
+//    static void Main()
+//    {
+//        President p1 = President.GetInstance();
+//        President p2 = President.GetInstance();
+//
+//
+//        if (p1 == p2)
+//        {
+//            Console.WriteLine("true");
+//        }
+//    }
+//}
---
+++ DesignPatterns/Structural.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+abstract class Abstraction
+{
+    protected Implementor implementor;
+    public Implementor Implementor
+    {
+        set { implementor = value; }
+    }
+    public Abstraction(Implementor imp)
+    {
+        implementor = imp;
+    }
+    public virtual void Operation()
+    {
+        implementor.OperationImp();
+    }
+}
+
+abstract class Implementor
+{
+    public abstract void OperationImp();
+}
+
+class RefinedAbstraction : Abstraction
+{
+    public RefinedAbstraction(Implementor imp)
+        : base(imp)
+    { }
+    public override void Operation()
+    {
+    }
+}
+
+class ConcreteImplementorA : Implementor
+{
+    public override void OperationImp()
+    {
+    }
+}
+
+class ConcreteImplementorB : Implementor
+{
+    public override void OperationImp()
+    {
+    }
+}
+
+
+class Client
+{
+    static void Main()
+    {
+        Abstraction abstraction;
+        abstraction = new RefinedAbstraction(new ConcreteImplementorA());
+        abstraction.Operation();
+        abstraction.Implementor = new ConcreteImplementorB();
+        abstraction.Operation();
+    }
+}
